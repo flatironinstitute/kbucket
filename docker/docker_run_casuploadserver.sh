c775c1972652255a7b73fe4a4664595caf22e959
Jeremy Magland <jeremy.magland@gmail.com>
2018-10-29 16:34:01 -0400

    update docker

--- docker/docker_run_casuploadserver.sh
+++ docker/docker_run_casuploadserver.sh
@@ -5,12 +5,10 @@
 #   Then decide which directory you are going to use for the share. Let's call it /my/share
 #   Create a /my/share/.env with the following content: (no leading spaces)
 #
-#   CAS_UPLOAD_DIRECTORY=/my/share
 #   CAS_UPLOAD_TOKEN=[some-secret-token-of-your-choice]
-#   PORT=[some-listen-port]
 #
 #   Then run this script as follows:
-#   ./docker_run_casuploadserver.sh /my/share [same-listen-port-as-above]
+#   ./docker_run_casuploadserver.sh /my/share [listen-port]
 
 
-docker run -v $1:/share -p $2:$2 -it magland/kbucket bash /scripts/casuploadserver.sh
+docker run -v $1:/share -p $2:24341 -it magland/kbucket bash /scripts/casuploadserver.sh
--- docker/scripts_inside_docker/casuploadserver.sh
+++ docker/scripts_inside_docker/casuploadserver.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+export PORT=24341
+export CAS_UPLOAD_DIR=/share
+
 cd /share
 /src/bin/casuploadserver
