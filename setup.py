70617eee53aafdc9ceb55949073acd469fe32044
Jeremy Magland <jeremy.magland@gmail.com>
2018-11-02 13:44:24 -0400

    incr

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ pkg_name="kbucket"
 
 setuptools.setup(
     name=pkg_name,
-    version="0.12.2",
+    version="0.12.3",
     author="Jeremy Magland",
     author_email="jmagland@flatironinstitute.org",
     description="Python client for kbucket",
